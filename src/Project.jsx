5599f4e037da772f2c40d87460eb367a76c27a81
nickoconnor99 <nickoconnor@gmail.com>
2024-05-23 15:53:05 +0100

    Added CSS

--- src/Project.jsx
+++ src/Project.jsx
@@ -12,15 +12,13 @@ function Project() {
         {projects.map((proj) => {
             return (
                 <section className='projects'>
-                <div className='title'>
                 <h2>projects</h2>
-                </div>
                 <div className='projects-center'>
                 {projects.map((item) => {
                     const {title, img, url, id} = item
                     return (
                         <a key={id} href={url} target='_blank' rel='noreferrer' className='project'>
-                        <img src={img}></img>
+                        <img src={img} className='img'></img>
                         <h5>{title}</h5>
                         </a>
                     )
